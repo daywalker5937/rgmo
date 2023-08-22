b2e589d91b562f42a6cc4e05d74ff9ba9253df39
DOF-edward <johnedward.rosas11@gmail.com>
2023-08-22 22:46:36 +0800

    renamed to php extension need to use php tags

---
+++ components/sidebar.php
@@ -0,0 +1,66 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+
+    <!-- Brand Logo -->
+    <div class="brand-link">
+
+      <div class="row justify-content-center mb-2 mt-3">
+        <img src="../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      </div>
+
+      <div class="row justify-content-center"><span class="brand-text font-weight-light h6">Resource Generation Management</span></div>
+      <div class="row justify-content-center"><span class="brand-text font-weight-light h6">Office<i>(RGMO)</i> Rental Services</span></div>
+      <div class="row justify-content-center"><span class="brand-text font-weight-light h6">Monitoring System</span></div>
+
+    </div>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+
+          <!-- Add icons to the links using the .nav-icon class with font-awesome or any other icon font library -->
+
+          <li class="nav-item">
+            <a href="#" class="nav-link active">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>Dashboard</p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-map-marker"></i>
+              <p>Location</p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-building"></i>
+              <p>Leaseholder</p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-bolt"></i>
+              <p>Services</p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-chart-bar"></i>
+              <p>Reports</p>
+            </a>
+          </li>
+
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
