aed49119695cd7e5a11850a63dd72ba7e9599139
DOF-edward <johnedward.rosas11@gmail.com>
2023-08-28 07:41:44 +0800

    get all services for sidebar

---
+++ objects/services.php
@@ -0,0 +1,33 @@
+<?php
+
+class Services {
+
+    // properties
+    public $service_id;
+    public $service_name;
+    public $location;
+    public $price;
+    public $description;
+    public $availability_status;
+    public $services;
+
+    // connection
+    private $conn;
+
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+    public function get_services() {
+
+        $query = "SELECT * FROM tbl_type_of_service";
+        $stmt = $this->conn->prepare($query);
+        $stmt->closeCursor();
+        $stmt->execute();
+        $this->services = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    }// get_services
+
+}// class
+
+?>
