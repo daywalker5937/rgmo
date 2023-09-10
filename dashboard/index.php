c6f433c2100982868e5591eb3710fa0ca289a588
DOF-edward <johnedward.rosas11@gmail.com>
2023-09-10 09:11:32 +0800

    Admin Dashboard Small Boxes display

--- dashboard/index.php
+++ dashboard/index.php
@@ -50,10 +50,9 @@
                     <!-- Admin Dashboard View -->
                     <?php if($SES->role_name == 'admin') { ?>
 
-                        <!-- Small boxes (Stat box) -->
-                        <div class="row">
+                        <div class="row justify-content-center">
 
-                            <div class="col-lg-3 col-6">
+                            <div class="col-lg-3 col-4">
                                 <!-- small box -->
                                 <div class="small-box bg-info">
                                 <div class="inner">
@@ -69,48 +68,69 @@
                             </div>
                             <!-- ./col -->
 
-                            <div class="col-lg-3 col-6">
+                            <div class="col-lg-3 col-4">
                                 <!-- small box -->
-                                <div class="small-box bg-success">
+                                <div class="small-box bg-info">
                                 <div class="inner">
-                                    <h3>53<sup style="font-size: 20px">%</sup></h3>
+                                    <h3>150</h3>
 
-                                    <p>Bounce Rate</p>
+                                    <p>New Orders</p>
                                 </div>
                                 <div class="icon">
-                                    <i class="ion ion-stats-bars"></i>
+                                    <i class="ion ion-bag"></i>
                                 </div>
                                 <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                                 </div>
                             </div>
                             <!-- ./col -->
 
-                            <div class="col-lg-3 col-6">
+                            <div class="col-lg-3 col-4">
                                 <!-- small box -->
-                                <div class="small-box bg-warning">
+                                <div class="small-box bg-info">
                                 <div class="inner">
-                                    <h3>44</h3>
+                                    <h3>150</h3>
 
-                                    <p>User Registrations</p>
+                                    <p>New Orders</p>
                                 </div>
                                 <div class="icon">
-                                    <i class="ion ion-person-add"></i>
+                                    <i class="ion ion-bag"></i>
                                 </div>
                                 <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                                 </div>
                             </div>
                             <!-- ./col -->
 
-                            <div class="col-lg-3 col-6">
-                                <!-- small box -->
-                                <div class="small-box bg-danger">
-                                <div class="inner">
-                                    <h3>65</h3>
+                        </div>
+                        <!-- /.row -->
 
-                                    <p>Unique Visitors</p>
+                        <div class="row justify-content-center">
+
+                            <div class="col-lg-3 col-4">
+                                <!-- small box -->
+                                <div class="small-box bg-info">
+                                <div class="inner">
+                                    <h3>150</h3>
+
+                                    <p>New Orders</p>
                                 </div>
                                 <div class="icon">
-                                    <i class="ion ion-pie-graph"></i>
+                                    <i class="ion ion-bag"></i>
+                                </div>
+                                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                                </div>
+                            </div>
+                            <!-- ./col -->
+
+                            <div class="col-lg-3 col-4">
+                                <!-- small box -->
+                                <div class="small-box bg-info">
+                                <div class="inner">
+                                    <h3>150</h3>
+
+                                    <p>New Orders</p>
+                                </div>
+                                <div class="icon">
+                                    <i class="ion ion-bag"></i>
                                 </div>
                                 <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                                 </div>
