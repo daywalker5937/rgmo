87861191ff0a3f66710ac3111d0b649e59f09b1e
DOF-edward <johnedward.rosas11@gmail.com>
2023-09-10 13:16:30 +0800

    Display All Clients

--- controller/ProfileController.php
+++ controller/ProfileController.php
@@ -9,10 +9,10 @@ header('Content-Type: application/json; charset=utf-8');
 
 include_once __DIR__ . '/../includes/config/database.php';
 
-function getInfo($id) {
+$DATABASE = new Database();
+$db = $DATABASE->getConnection();
 
-    $DATABASE = new Database();
-    $db = $DATABASE->getConnection();
+function getInfo($id, $db) {
 
     $query = "SELECT A.*, B.email FROM tbl_user_info as A
         LEFT JOIN tbl_user_login as B
@@ -29,10 +29,56 @@ function getInfo($id) {
 
 }// get info
 
+// Get All list of Clients
+function getAllClients($db) {
+
+    $data = [];
+
+    $q = "SELECT user_id FROM tbl_user_role WHERE role_id = ? ";
+
+    $r_id = 2;
+    $st = $db->prepare($q);
+    $st->bindParam(1, $r_id);
+    $st->closeCursor();
+    $st->execute();
+    $r = $st->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach($r as $k => $id) {
+
+        $query = "SELECT A.*, B.email FROM tbl_user_info as A
+            LEFT JOIN tbl_user_login as B
+            ON A.user_id = B.user_id
+            WHERE A.user_id = ?
+        ";
+
+        $stmt = $db->prepare($query);
+        $stmt->bindParam(1, $id['user_id']);
+        $stmt->closeCursor();
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // location
+        $row['location'] = 'location';
+
+        // action
+        $row['action'] = 'action';
+        
+        $data[] = $row;
+        
+    }// foreach
+
+    return json_encode(['data' => $data]);
+
+}// get all client
+
 switch($_POST['case']) {
 
     case 'get_info':
-        echo getInfo($_POST['id']);
+        echo getInfo($_POST['id'], $db);
+    break;
+
+    case 'get all clients':
+        echo getAllClients($db);
     break;
 
 }// switch
---
+++ tenants/index.php
@@ -0,0 +1,146 @@
+<?php
+
+    include_once __DIR__ . '/../objects/session.php';
+
+    $SES = Session::getInstance();
+
+    if($SES->id == null) {
+        header("location: ../login/");
+    }
+    else if(isset($SES->id) && $SES->role_name == 'admin') {
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Tenants</title>
+    <?php require_once __DIR__ . '/../components/link.html'; ?>
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+
+    <div class="wrapper">
+
+        <?php
+            require_once __DIR__ . '/../components/navbar.php';
+            require_once __DIR__ . '/../components/sidebar.php';
+        ?>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Tenants</h1>
+                    </div><!-- /.col -->
+                </div><!-- /.container-fluid -->
+            </div>
+            <!-- /.content-header -->
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="container-fluid">
+
+                <div class="row">
+
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <button class="btn btn-primary"><i class="fas fa-plus"> Add New</i></button>
+                            </div>
+                            <!-- /.card-header -->
+                            <div class="card-body">
+                                <table id="tenants-table-id" class="table table-bordered table-striped"></table>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                    <!-- /.col-12 -->
+
+                </div>
+                <!-- /.row -->
+
+                </div><!-- /.container-fluid -->
+            </section>
+            <!-- /.content -->
+
+        </div>
+        <!-- /.content-wrapper -->
+
+        <?php require_once __DIR__ . '/../components/footer.html'; ?>
+
+        <!-- Control Sidebar -->
+        <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+        </aside>
+        <!-- /.control-sidebar -->
+
+    </div>
+    <!-- ./wrapper -->
+
+    <?php require_once __DIR__ . '/../components/script.html'; ?>
+
+</body>
+</html>
+
+<script>
+
+    $(document).ready(function() {
+
+        // Assign Role to js variable
+        let user_id = "<?php echo $SES->id; ?>";
+        let role = "<?php echo $SES->role_name; ?>";
+        displaySidebar(role, 'Tenants');
+
+        $('#tenants-table-id').DataTable({
+            "responsive": true,
+            "autoWidth": false,
+            "lengthChange": false,
+            ajax: {
+                url: '../controller/ProfileController.php',
+                type: 'POST',
+                data: {case: 'get all clients'}
+            },
+            columns: [
+
+                {title: 'Last Name', 'data': 'last_name', targets: [0]},
+                {title: 'First Name', 'data': 'first_name', targets: [1]},
+                {title: 'Address', 'data': 'address', targets: [2]},
+                {title: 'Email', 'data': 'email', targets: [3]},
+                {title: 'Phone Number', 'data': 'contact_number', targets: [4]},
+                {title: 'Location', 'data': 'location', targets: [5]},
+                {title: 'Action', 'data': 'action', targets: [6]},
+
+            ],
+            createdRow: function(row, data, index) {
+
+                // Location Button
+                $('td', row).eq(5).text('').append("<button class='btn btn-primary'>View</button>");
+
+                // Action Button
+                let action_buttons = 
+                $(
+                    "<div>" +
+                        "<button class='btn btn-success mr-2'>Edit</button>" +
+                        "<button class='btn btn-danger'>Delete</button>" +
+                    "</div>"
+                );
+                $('td', row).eq(6).text('').append(action_buttons);
+
+            }
+        });
+
+    });// document
+
+</script>
+
+<?php
+    }else {
+        header("location: ../objects/logout.php");
+    }
+?>
