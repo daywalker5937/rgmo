cace93d8ca0da9f1328838d58a940122851507a1
DOF-edward <johnedward.rosas11@gmail.com>
2023-09-16 18:06:57 +0800

    Added condition to event if confirm password is true in event listener

--- components/pass-show-hide.js
+++ components/pass-show-hide.js
@@ -1,8 +1,8 @@
 
 // SHOW PASSWORD
 const togglePass = document.querySelector('#togglePass');
-const toggleConfirm = document.querySelector('#toggleConfirm');
 const password = document.querySelector('#_pass');
+const toggleConfirm = document.querySelector('#toggleConfirm');
 const confirmPassword = document.querySelector('#_confirm-pass');
 
 togglePass.addEventListener('click', function (e) {
@@ -11,22 +11,26 @@ togglePass.addEventListener('click', function (e) {
     this.classList.toggle('fa-eye-slash');
 });
 
-toggleConfirm.addEventListener('click', function (e) {
-    const type = confirmPassword.getAttribute('type') === 'password' ? 'text' : 'password';
-    confirmPassword.setAttribute('type', type);
-    this.classList.toggle('fa-eye-slash');
-});
+if(toggleConfirm) {
+    toggleConfirm.addEventListener('click', function (e) {
+        const type = confirmPassword.getAttribute('type') === 'password' ? 'text' : 'password';
+        confirmPassword.setAttribute('type', type);
+        this.classList.toggle('fa-eye-slash');
+    });
+}
 
 // IF CAPSLOCK IS ON
 var input_pass = document.getElementById("_pass");
-var input_confirm = document.getElementById("_confirm-pass");
 var pass_caps = document.getElementById("caps-pass");
+var input_confirm = document.getElementById("_confirm-pass");
 var pass_confirm = document.getElementById("caps-confirm");
 
 input_pass.addEventListener("keyup", function(event) {
     (event.getModifierState("CapsLock")) ? pass_caps.style.display = "block" : pass_caps.style.display = "none"
 });
 
-input_confirm.addEventListener("keyup", function(event) {
-    (event.getModifierState("CapsLock")) ? pass_confirm.style.display = "block" : pass_confirm.style.display = "none"
-});
+if(input_confirm) {
+    input_confirm.addEventListener("keyup", function(event) {
+        (event.getModifierState("CapsLock")) ? pass_confirm.style.display = "block" : pass_confirm.style.display = "none"
+    });
+}
