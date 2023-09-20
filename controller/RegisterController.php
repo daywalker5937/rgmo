d89957886351164483dba60617883f278544f5e7
DOF-edward <johnedward.rosas11@gmail.com>
2023-09-20 22:15:35 +0800

    Submit Request without login

--- controller/RegisterController.php
+++ controller/RegisterController.php
@@ -9,109 +9,176 @@ header('Content-Type: application/json; charset=utf-8');
 
 include_once __DIR__ .  '/../includes/config/database.php';
 
-try {
+// Register Admin or Client
+function register_account() {
 
-    $target_dir = __DIR__ . "/../includes/images/";
-    $target_file = $target_dir . basename($_FILES["upload_pic"]["name"]);
-    $last_name = $_POST['lname'];
-    $first_name = $_POST['fname'];
-    $middle_name = $_POST['mname'];
-    $address = $_POST['address'];
-    $civil_status = $_POST['civil_status'];
-    $sex = $_POST['sex'];
-    $contact_number = $_POST['contact_number'];
-    $email_address = $_POST['email'];
-    $pass = $_POST['password'];
-    $con_pass = $_POST['confirm_password'];
-    $file_name = $_FILES["upload_pic"]["name"];
+    try {
 
-    // Check if file already exists
-    if (file_exists($target_file)) {
-        throw new Exception("File Name Already Exists! Try other Name!");
-    }
-
-    $DATABASE = new Database();
-    $DATABASE->getConnection();
-
-    // Check Email if Existing
-    $email_query = "SELECT email FROM tbl_user_login WHERE email = ? ";
-    $stmt_email = $DATABASE->connection->prepare($email_query);
-    $stmt_email->bindParam(1, $email_address);
-    $stmt_email->closeCursor();
-    $stmt_email->execute();
-    $email_row = $stmt_email->fetch(PDO::FETCH_ASSOC);
-
-    // If Email Address Submitted is not existing
-    if(!$email_row) {
-
-        // If Password is Same with Confirm Password
-        if($pass == $con_pass) {
-
-            // Upload Image
-            if(move_uploaded_file($_FILES["upload_pic"]["tmp_name"], $target_file)) {
-            
-                /** Create User Account **/
-
-                // hash password
-                $hashed_password = password_hash($pass, PASSWORD_DEFAULT);  
-                
-                // Insert to user_info table
-                $info_query = "INSERT INTO tbl_user_info 
-                    (first_name, last_name, middle_name, address, contact_number, sex, user_image, civil_status)
-                    VALUES (?,?,?,?,?,?,?,?)
-                ";
-
-                $stmt_info = $DATABASE->connection->prepare($info_query);
-                $stmt_info->bindParam(1, $first_name);
-                $stmt_info->bindParam(2, $last_name);
-                $stmt_info->bindParam(3, $middle_name);
-                $stmt_info->bindParam(4, $address);
-                $stmt_info->bindParam(5, $contact_number);
-                $stmt_info->bindParam(6, $sex);
-                $stmt_info->bindParam(7, $file_name);
-                $stmt_info->bindParam(8, $civil_status);
-                $stmt_info->closeCursor();
-                $stmt_info->execute();
-
-                // Get User Id of User
-                $user_id = $DATABASE->connection->lastInsertId();
-                
-                // Insert to login table
-                $login_query = "INSERT INTO tbl_user_login (email, password, user_id) VALUES (?,?,?) ";
-                $stmt_login = $DATABASE->connection->prepare($login_query);
-                $stmt_login->bindParam(1, $email_address);
-                $stmt_login->bindParam(2, $hashed_password);
-                $stmt_login->bindParam(3, $user_id);
-                $stmt_login->closeCursor();
-                $stmt_login->execute();
-
-                // Insert to a Client Role to tbl_user_role
-                $role_id = $_POST['role'];
-                $role_query = "INSERT INTO tbl_user_role (user_id, role_id) VALUES (?,?)";
-                $stmt_role = $DATABASE->connection->prepare($role_query);
-                $stmt_role->bindParam(1, $user_id);
-                $stmt_role->bindParam(2, $role_id);
-                $stmt_role->closeCursor();
-                $stmt_role->execute();
-
-                echo json_encode(['status' => true]);
-
-            }else {
-                throw new Exception("Something wrong with uploading your image! Please Try Again!");
-            }
-
-        }// if passwords are same
-
-    }// if not existing
-    else {
-        throw new Exception("Email already used by other user! Try other Email Address!");
-    }
-
-}catch(Exception $e) {
-    echo json_encode(['status' => false, 'message' => $e->getMessage()]);
-}
-
-
+        $target_dir = __DIR__ . "/../includes/images/";
+        $target_file = $target_dir . basename($_FILES["upload_pic"]["name"]);
+        $last_name = $_POST['lname'];
+        $first_name = $_POST['fname'];
+        $middle_name = $_POST['mname'];
+        $address = $_POST['address'];
+        $civil_status = $_POST['civil_status'];
+        $sex = $_POST['sex'];
+        $contact_number = $_POST['contact_number'];
+        $email_address = $_POST['email'];
+        $pass = $_POST['password'];
+        $con_pass = $_POST['confirm_password'];
+        $file_name = $_FILES["upload_pic"]["name"];
     
+        // Check if file already exists
+        if (file_exists($target_file)) {
+            throw new Exception("File Name Already Exists! Try other Name!");
+        }
+    
+        $DATABASE = new Database();
+        $DATABASE->getConnection();
+    
+        // Check Email if Existing
+        $email_query = "SELECT email FROM tbl_user_login WHERE email = ? ";
+        $stmt_email = $DATABASE->connection->prepare($email_query);
+        $stmt_email->bindParam(1, $email_address);
+        $stmt_email->closeCursor();
+        $stmt_email->execute();
+        $email_row = $stmt_email->fetch(PDO::FETCH_ASSOC);
+    
+        // If Email Address Submitted is not existing
+        if(!$email_row) {
+    
+            // If Password is Same with Confirm Password
+            if($pass == $con_pass) {
+    
+                // Upload Image
+                if(move_uploaded_file($_FILES["upload_pic"]["tmp_name"], $target_file)) {
+                
+                    /** Create User Account **/
+    
+                    // hash password
+                    $hashed_password = password_hash($pass, PASSWORD_DEFAULT);  
+                    
+                    // Insert to user_info table
+                    $info_query = "INSERT INTO tbl_user_info 
+                        (first_name, last_name, middle_name, address, contact_number, sex, user_image, civil_status)
+                        VALUES (?,?,?,?,?,?,?,?)
+                    ";
+    
+                    $stmt_info = $DATABASE->connection->prepare($info_query);
+                    $stmt_info->bindParam(1, $first_name);
+                    $stmt_info->bindParam(2, $last_name);
+                    $stmt_info->bindParam(3, $middle_name);
+                    $stmt_info->bindParam(4, $address);
+                    $stmt_info->bindParam(5, $contact_number);
+                    $stmt_info->bindParam(6, $sex);
+                    $stmt_info->bindParam(7, $file_name);
+                    $stmt_info->bindParam(8, $civil_status);
+                    $stmt_info->closeCursor();
+                    $stmt_info->execute();
+    
+                    // Get User Id of User
+                    $user_id = $DATABASE->connection->lastInsertId();
+                    
+                    // Insert to login table
+                    $login_query = "INSERT INTO tbl_user_login (email, password, user_id) VALUES (?,?,?) ";
+                    $stmt_login = $DATABASE->connection->prepare($login_query);
+                    $stmt_login->bindParam(1, $email_address);
+                    $stmt_login->bindParam(2, $hashed_password);
+                    $stmt_login->bindParam(3, $user_id);
+                    $stmt_login->closeCursor();
+                    $stmt_login->execute();
+    
+                    // Insert to a Client Role to tbl_user_role
+                    $role_id = $_POST['role'];
+                    $role_query = "INSERT INTO tbl_user_role (user_id, role_id) VALUES (?,?)";
+                    $stmt_role = $DATABASE->connection->prepare($role_query);
+                    $stmt_role->bindParam(1, $user_id);
+                    $stmt_role->bindParam(2, $role_id);
+                    $stmt_role->closeCursor();
+                    $stmt_role->execute();
+    
+                    return json_encode(['status' => true]);
+    
+                }else {
+                    throw new Exception("Something wrong with uploading your image! Please Try Again!");
+                }
+    
+            }// if passwords are same
+    
+        }// if not existing
+        else {
+            throw new Exception("Email already used by other user! Try other Email Address!");
+        }
+    
+    }catch(Exception $e) {
+        return json_encode(['status' => false, 'message' => $e->getMessage()]);
+    }
+
+}// register
+
+function request_account() {
+
+    try {
+
+        $DATABASE = new Database();
+        $DATABASE->getConnection();
+
+        // Save Client Info
+        $info_query = "INSERT INTO tbl_user_info (first_name, last_name, middle_name, address, contact_number, civil_status)
+            VALUES(?,?,?,?,?,?)";
+        
+        $info_stmt = $DATABASE->connection->prepare($info_query);
+        $info_stmt->bindParam(1, $_POST['fname']);
+        $info_stmt->bindParam(2, $_POST['lname']);
+        $info_stmt->bindParam(3, $_POST['mname']);
+        $info_stmt->bindParam(4, $_POST['address']);
+        $info_stmt->bindParam(5, $_POST['contact']);
+        $info_stmt->bindParam(6, $_POST['civil_status']);
+        $info_stmt->closeCursor();
+        $info_stmt->execute();
+
+        // Get User Id Created
+        $user_id = $DATABASE->connection->lastInsertId();
+
+        // Save Client Email
+        $login_query = "INSERT INTO tbl_user_login (email, user_id) VALUES(?,?)"; 
+        $login_stmt = $DATABASE->connection->prepare($login_query);
+        $login_stmt->bindParam(1, $_POST['email']);
+        $login_stmt->bindParam(2, $user_id);
+        $login_stmt->closeCursor();
+        $login_stmt->execute();
+
+        // Save Client Request Form
+        $status = 'Pending';
+        $query = "INSERT INTO tbl_client_form (client_id, status, service_id) VALUES(?,?,?)";
+        $stmt = $DATABASE->connection->prepare($query);
+        $stmt->bindParam(1, $user_id);
+        $stmt->bindParam(2, $status);
+        $stmt->bindParam(3, $_POST['type_id']);
+        $stmt->closeCursor();
+        $stmt->execute();
+
+        return json_encode(['status' => true]);
+
+    }catch(Exception $e) {
+        return json_encode(['status' => false, 'message' => $e->getMessage()]);
+    }
+
+
+}// request
+
+switch($_POST['case']) {
+
+    // Register Account of Client
+    case 'register':
+        echo register_account();
+    break;
+
+    case 'request':
+        echo request_account();
+    break;
+
+
+}// switch
 
 ?>
--- services/direct_service.php
+++ services/direct_service.php
@@ -9,7 +9,7 @@
 
 <body class="hold-transition sidebar-mini layout-fixed">
     <div class="row justify-content-center">
-        <div class="card m-4 col-10">
+        <div class="card m-4 col-10 border border-dark">
             <div class="card-header">
                 <div class="row">
                     <div class="col-sm-1">
@@ -18,7 +18,55 @@
                     <div class="col-sm-10"><h4></h4></div>
                 </div>
             </div>
-            <div class="card-body"></div>
+            <div class="card-body">
+                <?php if(isset($_GET['client']) && $_GET['client'] == 1) { ?>
+
+                    <form id="client-form-id">
+                    
+                        <div class="row mb-2"><h5>Client Information:</h5></div>
+
+                        <div class="row">
+                            <div class="form-group col-4">
+                                <input type="text" name="lname" class="form-control" placeholder="Surname">
+                            </div>
+                            <div class="form-group col-4">
+                                <input type="text" name="fname" class="form-control" placeholder="Given Name">
+                            </div>
+                            <div class="form-group col-4">
+                                <input type="text" name="mname" class="form-control" placeholder="Middle Name">
+                            </div>
+                        </div>
+                        <!-- /.row -->
+
+                        <div class="row">
+                            <div class="form-group col-8">
+                                <input type="text" name="address" class="form-control" placeholder="Address">
+                            </div>
+                            <div class="form-group col-4">
+                                <select name="civil_status"><option value=""></option>
+                                </select>
+                            </div>
+                        </div>
+                        <!-- /.row -->
+
+                        <div class="row">
+                            <div class="form-group col-6">
+                                <input type="email" name="email" class="form-control" placeholder="Email">
+                            </div>
+                            <div class="form-group col-6">
+                                <input type="number" name="contact" class="form-control" placeholder="Contact Number">
+                            </div>
+                        </div>
+                        <!-- /.row -->
+
+                        <div class="float-right">
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </div>
+
+                    </form>
+
+                <?php } ?>
+            </div>
         </div>
         <!-- /.card -->
     </div>
@@ -32,10 +80,53 @@
 
         let service_name = GetURLParameter('service_name');
         let type_id = GetURLParameter('type_id');
+        let client_bool = GetURLParameter('client');
+        let civil_selection = [
+            {"id": 'Single', "text": 'Single'},{"id": 'Married', "text": 'Married'},
+            {"id": 'Divorced', "text": 'Divorced'},{"id": 'Widowed', "text": 'Widowed'}
+        ];
 
+        // Select Sub Service
+        if(type_id == 0 && client_bool == 0) {
+            
+            // Header Display
+            $('h4').text('Services/' + service_name);
 
-        // If process is for service_info
-        if(type_id) {
+            // List of Services Available
+            $.ajax({
+                url: '../controller/ServicesController.php',
+                type: 'POST',
+                data: {case: 'fetch type', service_name: service_name},
+                success: function(data) {
+
+                    let row = $("<div class='row'></div>");
+                    
+                    data.forEach(element => {
+                        
+                        let div_col = $("<div class='col-sm-6'></div>");
+                        let img = $("<img src='../includes/images/"+element.service_image+"' alt='service image' class='border border-dark' height='200' width='50%'>")
+                            .css('cursor', 'pointer')
+                            .on('click', () => { 
+                                window.location.href = 'direct_service.php?service_name=' + service_name + '&type_id=' + element.type_id + '&client=0' 
+                            });
+                        let p_name = $("<p class='mb-0'>"+ element.type_name +"</p>");
+                        let p_availability = $("<p> Available: "+ element.availability_status +"</p>");
+
+                        div_col.append(img).append(p_name).append(p_availability);
+                        row.append(div_col);
+
+                    });
+
+                    // Display Images to card
+                    $('.card-body').append(row);
+
+                }
+            });// ajax
+
+        }// if sub service is on
+
+        // Display Info of Selected Sub Service
+        if(type_id > 0 && client_bool == 0) {
 
             // Header Display
             $('h4').text('Services/' + service_name + '/Information');
@@ -67,9 +158,15 @@
                                     "<div class='row mt-2'> <b class='mr-1'>Location:</b> " + data.location + "</div>" +
                                 "</div>" +
                                 "<div style='width: 80%'>" +
-                                    "<div class='row mt-2 float-right'> <a href='client_form.php?type_id='"+ data.type_id +"' class='btn btn-primary'>I Choose This</a> </div>" +
+                                    "<div class='row mt-2 float-right'> " + 
+                                        "<a href='direct_service.php?service_name="+service_name+"&type_id="+type_id+"&client=1' class='btn btn-primary'>" +
+                                        "I Choose This</a>" +
+                                    "</div>" +
                                 "</div>" +
                             "</div>"
+
+                            // window.location.href = 'direct_service.php?service_name=' + service_name + '&type_id=' + element.type_id 
+
                         );
 
                     // Image
@@ -86,44 +183,106 @@
                 }
             });// ajax
 
-        }
-        else {
+        }// if display info selected sub service
 
-            // Header Display
-            $('h4').text('Services/' + service_name);
+        // Client Submitting Form
+        if(client_bool == 1) {
 
-            // List of Services Available
-            $.ajax({
-                url: '../controller/ServicesController.php',
-                type: 'POST',
-                data: {case: 'fetch type', service_name: service_name},
-                success: function(data) {
+            // Initialize Civil Status
+            let cs_select = $('#client-form-id select');
 
-                    let row = $("<div class='row'></div>");
-                    
-                    data.forEach(element => {
-                        
-                        let div_col = $("<div class='col-sm-6'></div>");
-                        let img = $("<img src='../includes/images/"+element.service_image+"' alt='service image' class='border border-dark' height='200' width='50%'>")
-                            .css('cursor', 'pointer')
-                            .on('click', () => { 
-                                window.location.href = 'direct_service.php?service_name=' + service_name + '&type_id=' + element.type_id 
+            // Header
+            $('h4').text('Hi Client!');
+
+            // Select2 cs
+            $('#client-form-id select').select2({
+                width: '100%',
+                theme: 'bootstrap4',
+                placeholder: 'Civil Status',
+                allowClear: true,
+                data: civil_selection
+            }).on('change', function() {($(this).val() == "") ? $(this).addClass('is-invalid') : $(this).removeClass('is-invalid') });
+
+            // Submit Client Data
+            $('#client-form-id').validate({
+                rules: {
+                    lname: {required: true},
+                    fname: {required: true},
+                    mname: {required: true},
+                    address: {required: true},
+                    civil_status: {required: true},
+                    email: {required: true},
+                    contact: {required: true, maxlength: 11, minlength: 11}
+                },
+                errorElement: 'span',
+                errorPlacement: function(error, element) {
+                    error.addClass('invalid-feedback');
+                    element.closest('.input-group').append(error);
+                    element.closest('.form-group').append(error);
+                },
+                highlight: function(element, errorClass, validClass) { $(element).addClass('is-invalid'); },
+                unhighlight: function(element, errorClass, validClass) { $(element).removeClass('is-invalid'); },
+                submitHandler: function(form) {
+
+                    Swal.fire({
+                        position: 'top',
+                        title: 'Are you sure!',
+                        text: 'You want to Submit this Request?',
+                        showCancelButton: true,
+                        confirmButtonColor: '#3085d6',
+                        cancelButtonColor: '#d33',
+                        confirmButtonText: 'Submit',
+                    }).then((result) => {
+
+                        if(result.isConfirmed) {
+                            
+                            let formData = new FormData(form);
+                            formData.append('case', 'request');
+                            formData.append('type_id', type_id);
+
+                            $.ajax({
+                                url: '../controller/RegisterController.php',
+                                type: 'POST',
+                                processData: false,
+                                contentType: false,
+                                data:formData,
+                                success: function(response) {
+
+                                    if(response.status == true) {
+
+                                        Swal.fire({
+                                            position: 'top',
+                                            icon: 'success',
+                                            title: 'Request Successfully Submitted!',
+                                            showConfirmButton: false,
+                                            timer: 1000
+                                        }).then(function() {
+                                            window.location.href = '../';
+                                        });
+
+                                    }
+                                    else {
+
+                                        Swal.fire({
+                                            position: 'top',
+                                            icon: 'warning',
+                                            title: response.message,
+                                            showConfirmButton: true
+                                        });
+
+                                    }
+
+                                }
                             });
-                        let p_name = $("<p class='mb-0'>"+ element.type_name +"</p>");
-                        let p_availability = $("<p> Available: "+ element.availability_status +"</p>");
+                            
+                        }// is confirmed
+                        
+                    });// swal
 
-                        div_col.append(img).append(p_name).append(p_availability);
-                        row.append(div_col);
+                }// submit handler
+            });// validate
 
-                    });
-
-                    // Display Images to card
-                    $('.card-body').append(row);
-
-                }
-            });// ajax
-
-        }// else        
+        }// if client is submitting form
 
     });// document ready
 
