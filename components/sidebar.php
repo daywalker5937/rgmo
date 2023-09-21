0bac223796fae58e6e4a71d9e2e75122b1f64ae1
DOF-edward <johnedward.rosas11@gmail.com>
2023-09-21 09:14:17 +0800

    Sidebar bg color class

--- components/sidebar.php
+++ components/sidebar.php
@@ -1,5 +1,5 @@
 <!-- Main Sidebar Container -->
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar sidebar-dark-light sidebar-bg-color elevation-4">
 
     <!-- Brand Logo -->
     <div class="brand-link">
